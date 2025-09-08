6b1e30dffcf09715ecd174986e5e4ffcb2553a11
hervendzye <hervendizeye0@gmail.com>
2025-09-08 20:12:34 +0200

    Solved Maple and Multiplucation  problem on codeforces

---
+++ Maple and multiplucation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int  a,b;
+        cin >> a>> b;
+        if(a<b)swap(a,b);
+        if(a%b == 0 && a!=b) cout << 1<< endl;
+        else if(a%b != 0 && a!=b) cout << 2<< endl;
+        else cout << 0<< endl;
+    }
+
+    return 0;
+}
