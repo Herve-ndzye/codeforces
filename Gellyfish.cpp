4c321cd0c14ccc6f89931f779f052f7b8b93cd72
hervendzye <hervendizeye0@gmail.com>
2025-09-15 12:12:33 +0200

    Solved Gellyfish and Flower problem on codeforces

---
+++ Gellyfish.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long a,b,c,d;
+        cin >> a>> b>> c>> d;
+        if (min(a,c) < min(b,d)) cout << "Flower\n";
+        else cout << "Gellyfish\n";
+    }
+    return 0;
+}
